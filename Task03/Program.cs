8ca0919a75524c85a684c7588be9e7b11370e84f
Yan Voronkov <sub.voronkov@gmail.com>
2022-12-21 21:39:23 +0300

    Add task03

---
+++ Task03/Program.cs
@@ -0,0 +1,41 @@
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+// 20 мин
+
+Console.WriteLine("Введите целое число от 1 до 7:");
+string day = Console.ReadLine();
+
+if (day == "1")
+{
+	Console.WriteLine("День недели - понедельник");
+}
+else if (day == "2")
+{
+	Console.WriteLine("День недели - вторник");
+}
+else if (day == "3")
+{
+	Console.WriteLine("День недели - среда");
+}
+else if (day == "4")
+{
+	Console.WriteLine("День недели - четверг");
+}
+else if (day == "5")
+{
+	Console.WriteLine("День недели - пятница");
+}
+else if (day == "6")
+{
+	Console.WriteLine("День недели - суббота");
+}
+else if (day == "7")
+{
+	Console.WriteLine("День недели - воскресенье");
+}
+else
+{
+	Console.WriteLine("Проверьте введенные данные");
+}
