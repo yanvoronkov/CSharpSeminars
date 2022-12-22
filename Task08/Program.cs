4e5730a64c7007a9b61d427ba215ad5a76e6e0a7
Yan Voronkov <sub.voronkov@gmail.com>
2022-12-22 09:29:11 +0300

    Add task08

---
+++ Task08/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+while (count < num)
+{
+	if (count % 2 == 0)
+	{
+		Console.Write($" {count}");
+	}
+	count++;
+}
