a55039bb1a06b722f8e790913c6d34db35653736
Yan Voronkov <sub.voronkov@gmail.com>
2022-12-22 21:34:40 +0300

    Add task23

---
+++ Task23/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 23
+// Напишите программу, которая
+// 1. принимает на вход число (N) и
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+void Numb(int n1)
+{
+	int counter = 1;
+	while (counter <= n1)
+	{
+		Console.WriteLine($"{counter,4} | {counter * counter * counter,4}");
+		counter++;
+	}
+}
+
+System.Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 1)
+{
+	System.Console.WriteLine("Введите положительное, целое число");
+	number = Convert.ToInt32(Console.ReadLine());
+}
+
+Numb(number);
