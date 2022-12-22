d797a6b53f71f40dc2a1582cbad00a9ad144208d
Yan Voronkov <sub.voronkov@gmail.com>
2022-12-22 10:05:29 +0300

    Add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0) Console.WriteLine("Число кратно 7 и 23");
+else Console.WriteLine("Число не кратно 7 и 21");
+
+bool DivCheck(int number)
+{
+	return number % 7 == 0 && number % 23 == 0;
+}
