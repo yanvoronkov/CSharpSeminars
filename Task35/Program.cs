659e3720dc3b8f22d4499569ada7c1579743f19b
Yan Voronkov <sub.voronkov@gmail.com>
2023-01-16 12:59:47 +0300

    Add Tasks 35, 37

---
+++ Task35/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 35: 
+// 1. Задайте одномерный массив из 123 случайных чисел.
+// 2. Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] CreatArray(int arraySize, int arrayMinValue, int arrayMaxValue)//Метод генерации массива со случайными элементами
+{
+	int[] resArray = new int[arraySize];
+	Random rnd = new Random();
+	for (int i = 0; i < resArray.Length; i++)
+	{
+		resArray[i] = rnd.Next(arrayMinValue, arrayMaxValue + 1);
+	}
+	return resArray;
+}
+
+string ArrayToString(int[] array)//Метод преобразования массива в строку
+{
+	string result = "[";
+	for (int i = 0; i < array.Length; i++)
+	{
+		result = result + $" {array[i]}";
+		result = i < (array.Length - 1) ? result = result + "," : result;//для корректности вывода запятых
+	}
+	return result + " ]";
+}
+
+int CountElements(int[] array, int fMin, int fMax)
+{
+	int result = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i] >= fMin && array[i] <= fMax) result++;
+	}
+	return result;
+}
+
+int arraySize = 6, arrayMinValue = 0, arrayMaxValue = 123, findMin = 10, findMax = 99;
+
+int[] newArray = CreatArray(arraySize, arrayMinValue, arrayMaxValue); //генерируем массив с указаными параметры
+string strArray = ArrayToString(newArray);// переводим строку в массив для дальнейшего вывода на экран
+
+int count = CountElements(newArray, findMin, findMax);
+Console.WriteLine($"{strArray} -> {count}");
---
+++ Task37/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] CreatArray(int arraySize, int arrayMinValue, int arrayMaxValue)//Метод генерации массива со случайными элементами
+{
+	int[] resArray = new int[arraySize];
+	Random rnd = new Random();
+	for (int i = 0; i < resArray.Length; i++)
+	{
+		resArray[i] = rnd.Next(arrayMinValue, arrayMaxValue + 1);
+	}
+	return resArray;
+}
+
+string ArrayToString(int[] array)//Метод преобразования массива в строку
+{
+	string result = "[";
+	for (int i = 0; i < array.Length; i++)
+	{
+		result = result + $" {array[i]}";
+		result = i < (array.Length - 1) ? result = result + "," : result;//для корректности вывода запятых
+	}
+	return result + " ]";
+}
+
+int[] MakeArray(int[] array) // Метод для создания массива на основе переданного. Согласно Т.З.
+{
+	int size = array.Length % 2 != 0 ? array.Length / 2 + 1 : array.Length / 2; //определяем размер нового массива. Нюнасы с четным и не четным кол-вом элементов
+	int[] resArray = new int[size];
+
+	for (int i = 0; i < resArray.Length; i++)
+	{
+		resArray[i] = array[i] * array[array.Length - i - 1];
+	}
+
+	if (array.Length % 2 == 1) resArray[size - 1] = array[array.Length / 2];
+	return resArray;
+}
+
+int arraySize = 6, arrayMinValue = 0, arrayMaxValue = 10;
+
+int[] newArray = CreatArray(arraySize, arrayMinValue, arrayMaxValue); //генерируем массив с указаными параметры
+string strArray = ArrayToString(newArray);// переводим строку в массив для дальнейшего вывода на экран
+
+int[] halfAndDouble = MakeArray(newArray); //Создаем новый массив на основании ранее сгенернируемого
+string strHalfAndDouble = ArrayToString(halfAndDouble);
+
+Console.WriteLine($"{strArray} -> {strHalfAndDouble}");
