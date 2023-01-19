a1eae1a43226ee42695b6b866f625852cc9ede3e
Yan Voronkov <sub.voronkov@gmail.com>
2023-01-19 09:02:39 +0300

    Add Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -4,6 +4,21 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-System.Console.WriteLine("Введите число:");
-int numberA = Convert.ToInt32(Console.ReadLine());
+int ExponentNum(int num, int exp)
+{
+	int result = 1;
+	for (int i = 0; i < exp; i++)
+	{
+		result = result * num;
+		Console.WriteLine(result);
+	}
+	return result;
+}
 
+System.Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите степень числа:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int exponentNum = ExponentNum(a, b);
+Console.WriteLine($"{a}, {b} -> {exponentNum}");
